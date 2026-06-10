08c8db8ab894d09c9d29a089c9cfee394bc41610
amymadden-bit <amy.madden@mixpanel.com>
2026-06-10 17:06:06 -0500

    Update _meta.tsx (#2748)
    * Update _meta.tsx
    
    Update left nav to highlight AI
    
    * Update _meta.tsx
    
    corrected urls
    
    * Update _meta.tsx
    
    error fix
    
    * Update _meta.tsx
    
    testing link
    
    * Update _meta.tsx
    
    test icon
    
    * Update _meta.tsx
    
    back to original
    
    * Update _meta.tsx
    
    test new section
    
    * Update _meta.tsx
    
    * Update _meta.tsx
    
    testing agent url
    
    * Update _meta.tsx
    
    remove /
    
    * Update _meta.tsx
    
    mirror changelog link
    
    * add href to nav bar
    
    ---------
    
    Co-authored-by: Myron Fung <myron.fung@mixpanel.com>
    Co-authored-by: myronkaifung <97630035+myronkaifung@users.noreply.github.com>

--- pages/guides/_meta.tsx
+++ pages/guides/_meta.tsx
@@ -29,6 +29,34 @@ export default {
   "guides-by-use-case": "Guides by Use Case",
   "guides-by-workflow": "Guides by Workflow",
   "guides-by-topic": "Guides by Topic",
+  "mixpanel-ai": {
+    type: "separator",
+    title: (
+      <div className={style.titleContainer}>
+        <DiscoverIcon /> MIXPANEL AI
+      </div>
+    ),
+  },
+  "mixpanel-agent": {
+      title: "Use Mixpanel Agent",
+      href: "/guides/guides-by-use-case/empower-your-team/mixpanel-agent",
+      newWindow: false
+    },
+  "mixpanel-mcp": {
+    title: "Use Mixpanel MCP",
+    href: "/guides/guides-by-use-case/empower-your-team/mcp",
+    newWindow: false
+  },
+  "mcp-by-industry": {
+    title: "MCP by Industry",
+    href: "/guides/guides-by-use-case/empower-your-team/mcp/mcp-by-industry",
+    newWindow: false
+  },
+  "mcp-integration": {
+    title: "MCP Integration",
+    href: "/guides/guides-by-use-case/empower-your-team/mcp/integrations",
+    newWindow: false
+  },
   "best-practices-and-playbooks": {
     type: "separator",
     title: (
